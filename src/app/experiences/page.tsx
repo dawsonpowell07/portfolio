d7d9b146205aac0f758468f8af74b52098be41f1
Dawson <dawsonpowell07@gmail.com>
2024-09-18 19:13:02 -0400

    updated wording

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -32,11 +32,11 @@ export default function Page() {
         </div>
         <ul className="list-inside list-disc space-y-1">
           <li>
-            I am currently a member of{" "}
+            I was a member of{" "}
             <span className="font-bold">
               Capital One&apos;s Technology Early Internship Program
             </span>
-            , which is specifically for freshman and sophomore students. During
+            . During
             the 10-week program, the first five weeks are spent learning best
             practices and various technologies used at Capital One, such as Go,
             Swift, React, AWS, and databases. I also got hands-on experience
@@ -44,53 +44,12 @@ export default function Page() {
           </li>
           <li>
             My remaining five weeks was spent working with multiple other
-            interns. We developed some internal autmoation that help standardize
+            interns. We developed some internal automation that helped standardize
             Jira Practices across different teams. We utilized{" "}
             <span className="font-bold">Python</span> and{" "}
             <span className="font-bold">AWS</span> to do this. During my short
             period there helped fix <span className="font-bold">1000+</span>{" "}
             issues regarding Jira Tickets.
-          </li>
-        </ul>
-        <hr className="border-muted" />
-      </section>
-
-      <section className="space-y-3">
-        <div className="flex items-center space-x-3">
-          <H2>Codepath Technical Interview Prep Member</H2>
-          <Image
-            src={codepath}
-            alt="Codepath Logo"
-            height={100}
-            width={100}
-            className="rounded-full border-2 object-cover shadow-md dark:border-foreground"
-          />
-        </div>
-        <ul className="list-inside list-disc space-y-1">
-          <p className="py-1">
-            The CodePath Technical Interview Prep Course is a comprehensive
-            program designed to equip students and professionals with the skills
-            needed to excel in technical interviews for software engineering
-            roles. The course focuses on fundamental computer science concepts
-            and problem-solving techniques that are commonly tested in coding
-            interviews.
-          </p>
-          <H3 className="underline">Key Features</H3>
-          <li>
-            <span className="font-bold">Structured Curriculum:</span> The course
-            offers a well-organized curriculum that covers essential topics such
-            as data structures, algorithms, and system design.
-          </li>
-          <li>
-            <span className="font-bold">Hands-On Practice:</span> Participants
-            engage in extensive hands-on coding practice with a variety of
-            problems, ranging from easy to advanced difficulty levels.
-          </li>
-          <li>
-            <span className="font-bold">Mock Interviews:</span> The program
-            includes mock interviews conducted by industry professionals. These
-            sessions simulate real interview conditions, providing valuable
-            feedback and helping participants build confidence.
           </li>
         </ul>
         <hr className="border-muted" />
@@ -138,7 +97,7 @@ export default function Page() {
         <p>
           EcoRide is a carpooling platform designed to promote sustainable
           transportation among college students. Collaborating with a team of
-          five, I developed a full-stack application utilizing Flask and
+          five, we developed a full-stack application utilizing Flask and
           Next.js, enabling real-time user interactions. This project aimed to
           reduce the carbon footprint on campus while offering a convenient and
           eco-friendly transportation alternative for students.
